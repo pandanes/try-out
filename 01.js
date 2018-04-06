1a6b4a0449159e93115785f0c9df9003d994088f
pandanes <37908766+pandanes@users.noreply.github.com>
2018-04-06 14:40:11 +0700

    Create 01.js
    Submit answer

---
+++ 01.js
@@ -0,0 +1,8 @@
+function reverseString(str) {
+    return str.split("").reverse().join("");
+    }
+
+reverseString("ibu ratna antar ubi");
+
+//to test:
+//reverseString("ibu1 ratna2 antar3 ubi4");
